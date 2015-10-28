8020f645ab6b8e1e1e86ec043af406dfea1bc93d
Martin Kearn <martin.kearn@microsoft.com>
2015-10-28 10:15:31 +0000

    Added further gulp tasks to end

--- end/GulpFile.js
+++ end/GulpFile.js
@@ -1,14 +1,16 @@
-var gulp = require('gulp'),
-    minifycss = require("gulp-minify-css");
-	
-gulp.task("minifycss_task", function () {
+var gulp = require('gulp');
+var minifycss = require("gulp-minify-css");
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task("css_task", function () {
 	gulp.src("css/*.css")
+	.pipe(autoprefixer())
 	.pipe(minifycss())
-	.pipe(gulp.dest("wwwroot"));
+	.pipe(gulp.dest("wwwroot"))
 });
 
 gulp.task('csswatch_task', function() {
-	gulp.watch('css/*.css', ['minifycss_task']);
+	gulp.watch('css/*.css', ['css_task']);
 });
 	
-gulp.task('default', [ 'minifycss_task', 'csswatch_task' ]);
+gulp.task('default', [ 'css_task', 'csswatch_task' ]);
